f284b1b0cda384af41c0af6141804e9e4543112c
Samuel Clitty <samclitty@gmail.com>
2020-09-15 23:37:48 -0700

    started .js script, added question object array

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
-console.log("Hello")
+console.log("Hello, Grader! pls be nice...")
+
+var questionsObjArr = [
+    {
+        question: " What’s the first thing you should do once you encounter an emergency or abnormal situation?",
+        answerA: "FLY THE PLANE",
+        answerB: "DECLARE AN EMERGENCY",
+        answerC: "SUMMON CAPT. SULLY",
+        answerD: "RUN THE APPROPRIATE CHECKLIST",
+        correctAnswer: "FLY THE PLANE"
+    },   
+    {
+        question: " What is NOT an example of the 5 ‘Hazardous Attitudes’ defined by the FAA?",
+        answerA: "ANTI-AUTHORITY",
+        answerB: "IMPULSIVITY",
+        answerC: "MACHO",
+        answerD: "TIMID",
+        correctAnswer: "TIMID"
+    },
+    {
+        question: " What are the factors to consider while running your 'IMSAFE' checklist before you fly?",
+        answerA: "ILLNESS",
+        answerB: "FATIGUE",
+        answerC: "EMOTION",
+        answerD: "ALL",
+        correctAnswer: "ALL"
+    },
+    {
+        question: " Risk mitigation is paramount to a safe operation, and running the ‘PAVE’ model can help detect threats you may encounter during your flight. What is an example of an ‘External Pressure’?",
+        answerA: "GPS IS INOPERATIVE",
+        answerB: "DESIRED RUNWAY CLOSED",
+        answerC: "SPECIAL PASSENGER",
+        answerD: "BAD WEATHER",
+        correctAnswer: "SPECIAL PASSENGER"
+    },
+    {
+        question: " 'Taking chances is foolish' is the antidote to what ‘Hazardous Attitude’?",
+        answerA: "INVULNERABILITY",
+        answerB: "MACHO",
+        answerC: "RESIGNATION",
+        answerD: "IMPULSIVITY",
+        correctAnswer: "MACHO"
+    }, 
+    {
+        question: " If you’re not instrument rated what would the ‘level of severity’ for flying into an area of IMC be?",
+        answerA: "CATASTROPHIC",
+        answerB: "NEGLIGIBLE",
+        answerC: "MARGINAL",
+        answerD: "CRITICAL",
+        correctAnswer: "CATASTROPHIC"
+    },
+    {
+        question: " What checklist should every pilot do to gauge their physical fitness to fly?",
+        answerA: "IMSAFE",
+        answerB: "PAVE",
+        answerC: "CRM",
+        answerD: "ADM",
+        correctAnswer: "IMSAFE"
+    },
+    {
+        question: " 'Do it quickly!' – is an example of which ‘Hazardous Attitude’?",
+        answerA: "IMPULSIVITY",
+        answerB: "ANTI-AUTHORITY",
+        answerC: "MACHO",
+        answerD: "RESIGNATION",
+        correctAnswer: "IMPULSIVITY"
+    },
+    {
+        question: " The pilot’s job is to manage risk, not create hazards. What is one way a pilot can manage ‘External Pressures’ safely?",
+        answerA: "LOWER PERSONAL MINIMUMS",
+        answerB: "IGNORE MINOR DAMAGE",
+        answerC: "STICK TO THE SCHEDULE",
+        answerD: "EXPECT AND PLAN FOR DELAYS",
+        correctAnswer: "EXPECT AND PLAN FOR DELAYS"
+    },
+    {
+        question: " What’s the average time it takes for a non-instrument rated pilot to get spatially disoriented after entering IMC, which often leads to a catastrophic conclusion?",
+        answerA: "18 minutes",
+        answerB: "27 minutes",
+        answerC: "3 minutes",
+        answerD: "9 minutes"
+    }
+]
